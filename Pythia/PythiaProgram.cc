02868d2fcd11a7c8d82e1406ba427f3cca3b026c
dromeroa <davidromeroabad@gmail.com>
2019-02-12 12:23:15 -0500

    Implement HEPMC

--- Pythia/PythiaProgram.cc
+++ Pythia/PythiaProgram.cc
@@ -15,6 +15,38 @@ pythia.readFile("PythiaProgram.cmnd");
 // Extract settings to be used in the main program.
 int nEvent = pythia.mode("Main:numberOfEvents");
 
+// Interface for conversion from Pythia8::Event to HepMC event.
+HepMC::Pythia8ToHepMC ToHepMC;
+  
+// Specify file where HepMC events will be stored.
+HepMC::IO_GenEvent ascii_io("PythiaProgram.hepmc", std::ios::out);
+
+// Begin event loop. Generate event. Skip if error.
+for (int iEvent = 0; iEvent < 100; ++iEvent) {
+      if (!pythia.next()) continue;
 
 
+// Construct new empty HepMC event and fill it.
+// Units will be as chosen for HepMC build; but can be changed
+// by arguments, e.g. GenEvt( HepMC::Units::GEV, HepMC::Units::MM)
+HepMC::GenEvent* hepmcevt = new HepMC::GenEvent();
+ToHepMC.fill_next_event( pythia, hepmcevt );
+
+// Write the HepMC event to file. Done with it.
+ascii_io << hepmcevt;
+delete hepmcevt;
+  
+// End of event loop. Statistics. Histogram.
 }
+pythia.stat();
+cout << mult;
+ 
+// Done.
+return 0;
+}
+
+
+
+
+
+
